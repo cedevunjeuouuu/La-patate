4b51f0b74426450a14720ca21034d443c03b53c3
Lucas <lucas.kominiarz@laposte.net>
2025-01-29 16:40:44 +0100

    animation

--- Plateformer/Assets/Scripts/PlayerScript.cs
+++ Plateformer/Assets/Scripts/PlayerScript.cs
@@ -1,20 +1,26 @@
 using System;
+using System.Collections;
+using NUnit.Framework.Constraints;
 using Unity.VisualScripting;
 using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     private Rigidbody2D rb;
-    private bool isGrounded = false;
+    [SerializeField]private bool isGrounded = false;
     public Transform checkSol;
     private float rayonSol = 0.3f;
     [SerializeField] private LayerMask sol;
+    private Animator animatorRef;
+    [SerializeField] private float timeForJumpAnim ;
+    private float count;
+
 
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+        animatorRef = GetComponent<Animator>();
     }
 
 
@@ -26,10 +32,17 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        count += 0.001f;
+        if (isGrounded && count > 0.1 )
+        {
+            animatorRef.SetBool("isJumping", false);
+        }
         float x = Input.GetAxis("Horizontal");
-
         if (isGrounded && Input.GetButtonDown("Jump"))
         {
+            count = 0;
+            animatorRef.Play("takeOff");
+            animatorRef.SetBool("isJumping", true);
             rb.AddForce(new Vector2(0,250));
         }
         
@@ -38,13 +51,19 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.Translate(x * moveSpeed * Time.deltaTime,0,0);
             transform.eulerAngles = new Vector2(0,0);
+            animatorRef.SetBool("isRunning", true);
         }
         
-        if (x < 0)
+        else if (x < 0)
         {
             transform.Translate(-x * moveSpeed * Time.deltaTime,0,0);
             transform.eulerAngles = new Vector3(0, 180, 0);
+            animatorRef.SetBool("isRunning", true);
         }
-    } 
+        else
+        {
+            animatorRef.SetBool("isRunning", false);
+        }
+    }
 }
 
